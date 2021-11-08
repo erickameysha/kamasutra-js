2845c8a53d2f2c0c5b1a6939973c374e4c467dee
Eric <ekameyshaaa@gmail.com>
2021-11-08 12:24:11 +0300

    Разбив коÐмпонентов, структура папок

---
+++ src/components/Profile/MyPosts/Post/Post.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import s from './MyPosts.module.css'
+
+const MyPosts = () => {
+    return <div>
+        My post
+        <div className={s.item}>
+            New post
+        </div>
+        <div className={s.posts}>
+            <div className={s.item}>
+                <img src="https://www.vokrug.tv/pic/person/2/b/f/4/2bf448098b7badf3b37e87c510da29bc.jpeg" alt=""/>
+                post 1
+            </div>
+            <div>
+                post 2
+            </div>
+        </div>
+    </div>
+
+}
+
+export default MyPosts;
